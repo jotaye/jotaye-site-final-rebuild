b870ae959107ca9dda750482d5420b0b7aca5d80
jotaye <jotayegroupllc@gmail.com>
2025-05-31 23:52:54 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
+  // La carpeta 'public/' ya está configurada por defecto como publicDir
+  // publicDir: "public",
 });
