a55cbd23e8d6bfd4ab3f612ba7d7de861b3f4c28
jotaye <jotayegroupllc@gmail.com>
2025-06-02 18:41:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  },
   resolve: {
     alias: {
-      // Permite usar rutas absolutas a /src si lo desearas
-      '@': '/src'
+      "@": "/src"
     }
-  }
+  },
+  base: "/"
 });
