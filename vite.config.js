0455209de107879b54e7417976a1f8b086caa834
jotaye <jotayegroupllc@gmail.com>
2025-06-02 19:31:37 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src"
-    }
+  // Para que el public/ assets queden accesibles en /assets/...
+  publicDir: "public",
+  build: {
+    rollupOptions: {
+      // Si en algún momento externalizas módulos, agrégalos aquí
+      external: [],
+    },
   },
-  base: "/"
 });
