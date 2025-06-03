0948deac25f6c84ffaf0a5050a684cd153bc3e57
jotaye <jotayegroupllc@gmail.com>
2025-06-02 22:06:19 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // importa Swiper.css + MainApp.css
+import "./index.css"; // importa Tailwind + MainApp + Swiper
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
