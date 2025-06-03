6ce70c23a616c0dd6f6db068947a880355b5c38d
jotaye <jotayegroupllc@gmail.com>
2025-06-02 22:10:10 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Indicamos que el directorio “public” será la raíz para servir estáticos
   publicDir: "public"
 });
