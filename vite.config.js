c9149e5362afd255221b8758a096f3537c04567b
jotaye <jotayegroupllc@gmail.com>
2025-06-02 22:05:14 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Para que el public/ assets queden accesibles en /assets/...
-  publicDir: "public",
-  build: {
-    rollupOptions: {
-      // Si en algún momento externalizas módulos, agrégalos aquí
-      external: [],
-    },
-  },
+  // Indicamos que el directorio “public” será la raíz para servir estáticos
+  publicDir: "public"
 });
